70b6758454c8f233721b543b63af9487b0ef5d8a
elanbariM <elanbari.mohamed@gmail.com>
2018-11-26 00:04:33 +0100

    ajout de codeEnseignement

--- backend/Enseignement/Enseignement.js
+++ backend/Enseignement/Enseignement.js
@@ -3,7 +3,7 @@ var db = require('../db');
 var Enseignement = {
     getenseignement: function (callback) {
 
-        return db.query('SELECT alias FROM enseignements \n' +
+        return db.query('SELECT alias, enseignements.codeEnseignement FROM enseignements \n' +
             'JOIN enseignements_groupes on enseignements.codeEnseignement=enseignements_groupes.codeEnseignement \n' +
             'WHERE codeRessource=68196799',callback);
     },
