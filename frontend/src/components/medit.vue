089b7470c52f192e807a7493c0276e05ee60694d
Mynghado <kaamec@gmail.com>
2018-11-26 19:31:01 +0100

    frontend update (data communication)

--- frontend/src/components/medit.vue
+++ frontend/src/components/medit.vue
@@ -7,36 +7,36 @@
           <div class="field">
             <label class="label">Date de la séance</label>
             <div class="control">
-              <input class="input" type="text" placeholder="Text input" v-bind:value="seance.date">
+              <input class="input" type="text" placeholder="Text input" v-bind:value="this.seance.date">
             </div>
           </div>
           <div class="field">
             <label class="label">Heure de début</label>
             <div class="control">
-              <input class="input" type="text" placeholder="Text input" v-bind:value="seance.heure">
+              <input class="input" type="text" placeholder="Text input" v-bind:value="this.seance.heure">
             </div>
           </div>
           <div class="field">
             <label class="label">Durée de la séance</label>
             <div class="control">
-              <input class="input" type="text" placeholder="Text input" v-bind:value="seance.duree">
+              <input class="input" type="text" placeholder="Text input" v-bind:value="this.seance.duree">
             </div>
           </div>
           <div class="field">
             <label class="label">Enseignement</label>
             <div class="control">
-              <input class="input" type="text" placeholder="Text input" v-bind:value="seance.codeEnseignement">
+              <input class="input" type="text" placeholder="Text input" v-bind:value="this.seance.titre">
             </div>
           </div>
           <div class="field">
             <label class="label">Commentaire</label>
             <div class="control">
-              <textarea class="textarea" placeholder="Textarea" v-bind:value="seance.commentaire"></textarea>
+              <textarea class="textarea" placeholder="Textarea" v-bind:value="selection.salle"></textarea>
             </div>
           </div>
           <div class="field is-grouped">
             <div class="control">
-              <button class="button is-link">Submit</button>
+              <button v-on:click.prevent="post" class="button is-link">Submit</button>
             </div>
             <div class="control">
               <button @click="$emit('mclose')" class="button is-text">Cancel</button>
@@ -57,7 +57,12 @@ export default {
     course: {
       title: '',
       start: '',
-      end: ''
+      end: '',
+      teacher: '',
+      room: '',
+      code: '',
+      hour: '',
+      time: ''
     }
   },
   data () {
@@ -87,66 +92,50 @@ export default {
     }
   },
   methods: {
+    post: function () {
+      console.log('post')
+      console.log('date' + this.seance.date)
+      console.log('heure' + this.seance.heure)
+      console.log('duree' + this.seance.duree)
+      console.log('codeEnseignement' + this.seance.codeEnseignement)
+      console.log('commentaire' + this.seance.commentaire)
+      axios.post('http://localhost:3000/seance', {
+        dateSeance: this.seance.date,
+        heureSeance: this.seance.heure,
+        dureeSeance: this.seance.duree,
+        codeEnseignement: '68438839',
+        commentaire: this.seance.commentaire
+      }).then(function (data) {
+        console.log(data)
+      })
+    },
     formatDate: function () {
-      this.seance.date = this.selection.start.format() + '.000Z'
-      console.log(this.seance.date)
-    },
-    formatHeure: function () {
-      var complete = this.selection.start.format().slice(11, 16)
+      var date = this.selection.start
 
-      var heure = complete.slice(0, 2)
-      if (heure < 10) {
-        heure = heure.slice(1, 2)
-      }
-
-      var minute = complete.slice(3, 5)
-
-      this.seance.heure = heure.toString() + minute.toString()
-    },
-    formatDuree: function () {
-      var complete = this.selection.end.format().slice(11, 16)
-
-      var heure = complete.slice(0, 2)
-      if (heure < 10) {
-        heure = heure.slice(1, 2)
-      }
-
-      var minute = complete.slice(3, 5)
-
-      var heureFin = heure.toString() + minute.toString()
-      this.seance.duree = (heureFin - this.seance.heure).toString()
+      date.stripTime()
+      this.seance.date = date.format()
     },
     formatCodeEnseignement: function () {
-      this.seance.codeEnseignement = this.selection.title.slice(0, 9)
+      this.seance.codeEnseignement = this.selection.codeEnseignement
       console.log(this.seance.codeEnseignement)
+      console.log('enseignements format' + this.enseignements.length)
+    },
+    findEnseignement: function () {
+      console.log('findEnseignement' + this.enseignements.length)
     },
     formatProfesseur: function () {
-      this.seance.codeEnseignement = this.selection.title.slice(10, 19)
-      console.log(this.selection.title.slice(10, 19))
+      this.seance.codeEnseignement = this.selection.prof
+      console.log(this.selection.prof)
     },
     formatSalle: function () {
       console.log(this.selection.title.slice(19, 42))
     },
     getProfesseur: function () {
+      console.log('mmh' + this.seance.codeEnseignement)
       axios.get('http://localhost:3000/professeur/' + this.seance.codeEnseignement)
         .then((response) => {
           for (var i = 0; i < response.data.length; i++) {
-            console.log(response.data[i])
-            var al = response.data[i].alias
-            var code = response.data[i].codeEnseignement
-
-            this.enseignements.push({alias: al, codeEnseignement: code})
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    },
-    addSeance: function () {
-      axios.post('http://localhost:3000/seance')
-        .then((response) => {
-          for (var i = 0; i < response.data.length; i++) {
-            console.log(response.data[i])
+            console.log('enseignements prof :' + response.data[i])
             var al = response.data[i].alias
             var code = response.data[i].codeEnseignement
 
@@ -159,26 +148,31 @@ export default {
     }
   },
   beforeMount () {
-    this.formatCodeEnseignement()
-    this.formatProfesseur()
-    this.formatSalle()
-    this.formatDate()
-    this.formatHeure()
-    this.formatDuree()
-
     axios.get('http://localhost:3000/enseignement')
       .then((response) => {
-        console.log(response.data.length)
+        console.log('nombre enseignements' + response.data.length)
         for (var i = 0; i < response.data.length; i++) {
           var al = response.data[i].alias
           var code = response.data[i].codeEnseignement
-
+          console.log('boucle?')
+          console.log(this.enseignements.length)
           this.enseignements.push({alias: al, codeEnseignement: code})
         }
       })
       .catch((error) => {
         console.log(error)
       })
+
+    this.seance.titre = this.selection.title
+    this.formatCodeEnseignement()
+    this.formatDate()
+    this.seance.heure = this.selection.hour
+    this.seance.duree = this.selection.time
+    console.log('test')
+    this.findEnseignement()
+  },
+  mounted () {
+
   }
 }
 </script>
